24145ac455a900a9b52bacd8060feb4978ddd888
KBG1 <102010837+KBG1@users.noreply.github.com>
2024-02-29 14:01:57 +0900

    [D3] Title: 규영이와 인영이의 카드게임, Time: 2,718 ms, Memory: 20,152 KB -BaekjoonHub

--- SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
+++ SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
@@ -4,76 +4,66 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static int[] gyuCard;
-	static int[] inCard;
-	static int[] modCard;
-	static boolean[] check, isSelected;
+	static int[] gyuCard, inCard;
+	static boolean[] check;
 	static int ans;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
 		StringTokenizer st;
+		int T = Integer.parseInt(br.readLine());
+
 		for (int tc = 1; tc <= T; tc++) {
-			gyuCard = new int[9]; // 9개의 카드 저장
-			modCard = new int[9];
+			gyuCard = new int[9];
 			inCard = new int[9];
-			isSelected = new boolean[9];
-			boolean[] cnt = new boolean[18];
+			check = new boolean[18];
 			st = new StringTokenizer(br.readLine());
+			ans = 0;
+			int idxInCard = 0;
 			for (int i = 0; i < 9; i++) {
 				gyuCard[i] = Integer.parseInt(st.nextToken());
-				cnt[gyuCard[i] - 1] = true;
-				; // 먼저 체크해주기
+				check[gyuCard[i] - 1] = true;
 			}
 
-			int idx = 0;
-
 			for (int i = 0; i < 18; i++) {
-				if (cnt[i]) {
-					continue;
-				} else {
-					modCard[idx++] = i + 1;
+				if (!check[i]) {
+					inCard[idxInCard++] = i + 1;
 				}
 			}
 
-			ans = 0;
+			permu(0, 0, 0);
 
-			permu(0);
-			sb.append("#" + tc + " " + ans + " " + (362880 - ans) + "\n");
-
+			System.out.println("#" + tc + " " + ans + " " + (362880 - ans));
 		}
-
-		System.out.println(sb.toString());
 
 	}
 
-	public static void permu(int cnt) {
-		if (cnt == 9) {
-			int gyuPoint = 0;
-			int inPoint = 0;
-			for (int i = 0; i < 9; i++) {
-				if (gyuCard[i] > inCard[i]) {
-					gyuPoint += gyuCard[i] + inCard[i];
-				} else {
-					inPoint += gyuCard[i] + inCard[i];
-				}
-			}
-			if (gyuPoint > inPoint) {
+	public static void permu(int idx, int gyuScore, int inScore) {
+		if (idx == 9) {
+			if (gyuScore > inScore) {
 				ans++;
 			}
 			return;
 		}
 
-		for (int i = 0; i < 9; i++) {
-			if (isSelected[i]) {
-				continue;
+		for (int i = idx; i < 9; i++) {
+			int temp = inCard[i];
+			inCard[i] = inCard[idx];
+			inCard[idx] = temp;
+			
+			int sum = gyuCard[idx] + inCard[idx];
+
+			if (gyuCard[idx] > inCard[idx]) {
+				permu(idx + 1, gyuScore + sum, inScore);
+			} else if (gyuCard[idx] < inCard[idx]) {
+				permu(idx + 1, gyuScore, inScore + sum);
+			} else {
+				permu(idx + 1, gyuScore, inScore);
 			}
-			inCard[cnt] = modCard[i];
-			isSelected[i] = true;
-			permu(cnt + 1);
-			isSelected[i] = false;
+			
+			temp = inCard[i];
+			inCard[i] = inCard[idx];
+			inCard[idx] = temp;
 		}
 	}
 
