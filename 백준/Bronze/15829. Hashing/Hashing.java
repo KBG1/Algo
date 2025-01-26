153959988054fe890fdb908d43ef7ca99cd6f3bf
Kim Beom Gyu <102010837+KBG1@users.noreply.github.com>
2025-01-26 17:59:55 +0900

    [Bronze II] Title: Hashing, Time: 112 ms, Memory: 14372 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,35 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
-    static Map<Character, Integer> alpha;
-    static double mod = 1234567891;
-    static double ans = 0;
+    static Map<Character, BigInteger> alpha;
+    static BigInteger ans;
+    static BigInteger[] bigArray;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         double L = Double.parseDouble(br.readLine());
         String str = br.readLine();
+        bigArray = new BigInteger[50];
+        BigInteger ans = new BigInteger("0");
+        BigInteger mod = new BigInteger("1234567891");
 
         alpha = new HashMap<>();
-        //알파벳 삽입
+        // 알파벳 삽입
         insertAlpha();
 
-        for(int i = 0; i < L; i++){
-            ans += (alpha.get(str.charAt(i)) * Math.pow(31, i));
+        BigInteger base = BigInteger.valueOf(31);
+        for (int i = 0; i < 50; i++) {
+            bigArray[i] = base.pow(i);
         }
 
-        System.out.println(String.format("%.0f", ans));
+        for (int i = 0; i < L; i++) {
+            ans = ans.add(alpha.get(str.charAt(i)).multiply(bigArray[i]));
+        }
+
+        ans = ans.mod(mod);
+
+        System.out.println(ans);
+
     }
 
     public static void insertAlpha() {
         for (int i = 0; i < 26; i++) {
             char key = (char) ('a' + i);
-            alpha.put(key, i + 1);
+            alpha.put(key, BigInteger.valueOf(i + 1));
         }
     }
 }
