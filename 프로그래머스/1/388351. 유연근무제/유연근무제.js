c699d839012e1cceed1b3baa6f52025c983a7763
Kim Beom Gyu <102010837+KBG1@users.noreply.github.com>
2025-02-17 15:50:02 +0900

    [level 1] Title: 유연근무제, Time: 2.54 ms, Memory: 37.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.js
@@ -0,0 +1,32 @@
+function solution(schedules, timelogs, startday) {
+    let answer = 0;
+    let late = false;
+    
+    //1. schedules를 반복한다. 
+    schedules.forEach((schedule, idx) => {
+        let day = startday;
+        let scheduleTime = (Math.floor(schedule / 100) * 60) + (schedule % 100);
+        late = false;
+        //2. timelog를 보면서, 지각했는지 체크한다.
+        for(let i = 0; i < 7; i++){
+            let time = (Math.floor(timelogs[idx][i] / 100) * 60) + (timelogs[idx][i] % 100);
+            //3. 지각을 하면, 바로 out (토, 일 제외)
+            if(time - scheduleTime > 10 && (startday <= 5)){
+                late = true;
+            }
+            //4. startday를 증가시키면서 값 계속 비교
+            startday++;
+            if(startday === 8){
+                startday = 1;
+            }
+        }
+        //5. 지각 안했으면 선물
+        if(!late){
+            answer++;
+        }
+        
+    })
+    
+    
+    return answer;
+}
