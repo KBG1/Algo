5269fcc6d170962cd8ef3458c3e0cec24747b7e4
Kim Beom Gyu <102010837+KBG1@users.noreply.github.com>
2025-03-19 14:35:37 +0900

    [Gold IV] Title: 타일 채우기, Time: 104 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Gold/2133. 타일 채우기/타일 채우기.java
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.java
@@ -3,31 +3,30 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        // 홀수는 배치 불가
+        if (N % 2 == 1) {
+            System.out.println(0);
+        } else {
+            int[] dp = new int[N + 1];
+            dp[0] = 1;
+            dp[2] = 3;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            if (N == 2) {
+                System.out.println(3);
+            } else {
+                for (int i = 4; i <= N; i++) {
+                    dp[i] = dp[i - 2] * 3;
+                    for (int j = i - 4; j >= 0; j -= 2) {
+                        dp[i] += (dp[j] * 2);
+                    }
+                }
 
-		int N = Integer.parseInt(br.readLine());
-		int[] dp = new int[N + 1];
-
-		if (N % 2 == 1) {
-			System.out.println(0);
-			return;
-		}
-
-		dp[0] = 1;
-		dp[1] = 0;
-		dp[2] = 3;
-
-		for (int i = 4; i <= N; i += 2) {
-			dp[i] = dp[i - 2] * dp[2];
-			for (int j = i - 4; j >= 0; j -= 2) {
-				dp[i] = dp[i] + (dp[j] * 2);
-			}
-		}
-
-		System.out.println(dp[N]);
-
-	}
+                System.out.println(dp[N]);
+            }
+        }
+    }
 
 }
